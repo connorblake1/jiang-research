b1cd7bd29b5bfad0ebacbfdac615cc72e0207f5f
Connor Blake <theconnorblake@gmail.com>
2024-09-04 21:24:19 -0500

    Functioning minimal example of diffrax-based optimal control optimizer with autograd on the parametrized controls for the Hamiltonian of a closed quantum system.

---
+++ FiniteGKP/controllers.py
@@ -0,0 +1,43 @@
+import equinox as eqx
+from jaxtyping import Array
+from abc import abstractmethod
+import jax.numpy as jnp
+
+class AbstractControl(eqx.Module):
+
+    @abstractmethod
+    def __call__(self, t: float) -> float:
+        pass
+
+    def graph(self, times: Array, axhandle, label: str) -> None:
+        values = jnp.array([self.__call__(time) for time in times])
+        axhandle.plot(times, values,label=label)
+
+
+class SinusoidalControl(AbstractControl):
+    a: Array
+    omega: Array
+    phi: Array
+    def __call__(self, t: float) -> float:
+        out = 0
+        for i in range(len(self.a)):
+            out = out + self.a[i]*jnp.sin(self.omega[i]*t + self.phi[i])
+        return out
+    
+class ConstantControl(AbstractControl):
+    k: float
+    def __init__(self,k):
+        self.k = k
+    def __call__(self, t: float):
+        return self.k
+
+
+class ControlVector(eqx.Module):
+    us: list[AbstractControl]
+
+    def __call__(self, t: float):
+        return jnp.array([u(t) for u in self.us])
+    
+    def __iter__(self):
+        return iter(self.us)
+
---
+++ FiniteGKP/optimizers.py
@@ -0,0 +1,155 @@
+import equinox as eqx
+from jaxtyping import Array
+from typing import Callable
+import diffrax as df
+import optax
+from functools import partial
+from abc import abstractmethod
+import jax.numpy as jnp
+import dynamiqs as dq
+from controllers import AbstractControl, ControlVector
+
+class AbstractSystem(eqx.Module):
+    dim: int
+    H_0: Array
+    H_M: list[Array]
+
+    @abstractmethod
+    def vector_field_term(self, t: float, y, args: dict, controls: ControlVector):
+        pass
+    
+    def run_simulation(
+            self,
+            ts: Array,
+            dt: float,
+            y0: Array,
+            u_M: ControlVector
+        ):
+        results = df.diffeqsolve(
+            terms=df.ODETerm(partial(self.vector_field_term, controls=u_M)),
+            solver=df.Dopri5(),
+            t0=ts[0],
+            t1=ts[-1],
+            dt0=dt,
+            y0=y0,
+            saveat=df.SaveAt(ts=ts),
+            max_steps=1_000_000
+        )
+        return results.ys
+
+class ClosedQuantumSystem(AbstractSystem):
+    def vector_field_term(self, t: float, y: Array, args: dict, controls: ControlVector):
+        field = self.H_0
+        for u_m, H_m in zip(controls, self.H_M):
+            field = field + H_m*u_m(t)
+        return (-1j*field) @ y
+    
+class OptimalController(eqx.Module):
+    system: AbstractSystem
+    controls: ControlVector
+    y0: Array
+    duration: float
+    dt_start: float
+    dt_save: float
+    y_final: Callable[Array, float]
+    y_statewise: Callable[Array, float]
+    u_statewise: Callable[Array, float]
+    times: Array
+
+    def __init__(self,
+            system,
+            controls,
+            y0,
+            duration,
+            dt_start,
+            dt_save,
+            y_final,
+            y_statewise,
+            u_statewise
+        ):
+        self.system = system
+        self.controls = controls
+        if len(list(self.controls)) != len(self.system.H_M):
+            raise ValueError("Incorrect number of control Hamiltonians or controls.")
+        self.y0 = y0
+        self.duration = duration
+        self.dt_save = dt_save
+        self.dt_start = dt_start
+        self.y_final = y_final
+        self.y_statewise = y_statewise
+        self.u_statewise = u_statewise
+        self.times = jnp.arange(0.0,self.duration, self.dt_save)
+
+    def run(self, controls):
+        return self.system.run_simulation(
+            ts=self.times,
+            dt=self.dt_start,
+            y0=self.y0,
+            u_M=controls
+        )
+
+    def loss(self, controls):
+        return jnp.real(self.penalty(self.run(controls)))
+
+    def penalty(self, yt):
+        # TODO jaxify
+        L = 0
+        for i in range(yt.shape[0]):
+            L += self.y_statewise(yt[i])
+            L += 0 # control penalty TODO
+        L += self.y_final(yt[-1])
+        return L
+
+    def optimize(
+            self,
+            N_steps: int,
+            learning_rate: float = .001,
+            verbosity: int = 0) -> None:
+        optim = optax.adam(learning_rate, nesterov=True)
+        opt_state = optim.init(self.controls)
+        ctrls = self.controls
+        
+        @eqx.filter_jit
+        def make_step(ctrls, opt_state):
+            loss_val, grads = eqx.filter_value_and_grad(self.loss)(ctrls)
+            updates, opt_state = optim.update(grads, opt_state)
+            ctrls = eqx.apply_updates(ctrls, updates)
+            return ctrls, opt_state, loss_val
+
+        for step in range(N_steps):
+            ctrls, opt_state, loss_val = make_step(ctrls, opt_state=opt_state)
+            if verbosity > 0:
+                if verbosity > 1:
+                    eqx.tree_pprint(ctrls, short_arrays=False)
+                print(f"Step: {step} Loss: {loss_val}")
+        
+        return OptimalController(
+            system=self.system,
+            controls=ctrls,
+            y0=self.y0,
+            duration=self.duration,
+            dt_start=self.dt_start,
+            dt_save=self.dt_save,
+            y_final=self.y_final,
+            y_statewise=self.y_statewise,
+            u_statewise=self.u_statewise
+        )
+
+    def plot(
+            self,
+            ax,
+            exp_ops: list = [],
+            exp_names: list[str] = [],
+            plot_controls: bool = True
+        ):
+        yt = self.run(self.controls)
+        exps = dq.expect(exp_ops, yt)
+        for i, name in enumerate(exp_names):
+            ax.plot(self.times,jnp.real(exps[i]),label=name)
+        if plot_controls:
+            for i, u_m in enumerate(self.controls):
+                u_m.graph(self.times,ax,fr"$u_{i+1}(t)$")
+        
+
+
+            
